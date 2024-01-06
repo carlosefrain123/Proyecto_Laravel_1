6ea90c6a05b1341277a8a499617a1068be702640
carlosefrain123 <carlosefrainchallo1@gmail.com>
2024-01-06 09:07:24 -0500

    06/01/2024 - 09:06 - Se termina el primer proyecto de Laravel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,95 +1,109 @@
-<!doctype html>
-<html lang="en">
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <!DOCTYPE html>
+    <html lang="en">
+
     <head>
         <title>Title</title>
         <!-- Required meta tags -->
         <meta charset="utf-8" />
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
         <!-- Bootstrap CSS v5.2.1 -->
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"
-        />
-        <link rel="stylesheet" href="{{asset('assets/estilos.css')}}">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+        <link rel="stylesheet" href="{{ asset('assets/estilos.css') }}">
     </head>
 
     <body>
         <section class="h-100 gradient-form" style="background-color: #eee;">
             <div class="container py-5 h-100">
-              <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-xl-10">
-                  <div class="card rounded-3 text-black">
-                    <div class="row g-0">
-                      <div class="col-lg-6">
-                        <div class="card-body p-md-5 mx-md-4">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-xl-10">
+                        <div class="card rounded-3 text-black">
+                            <div class="row g-0">
+                                <div class="col-lg-6">
+                                    <div class="card-body p-md-5 mx-md-4">
 
-                          <div class="text-center">
-                            <img src="{{ asset('assets/img/logo.jpg')}}"
-                              style="width: 185px;" alt="logo">
-                            <h4 class="mt-1 mb-5 pb-1">{{__('Login')}}</h4>
-                          </div>
+                                        <div class="text-center">
+                                            <img src="{{ asset('assets/img/logo.jpg')}}" style="width: 185px;"
+                                                alt="logo">
+                                            <h4 class="mt-1 mb-5 pb-1">{{__('Login')}}</h4>
+                                        </div>
 
-                          <form action="{{route('login')}}" method="POST">
-                            @csrf
-                            <p>{{__('Please login to your account')}}</p>
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
 
-                            <div class="form-outline mb-4">
-                              <label class="form-label" for="form2Example11">{{__('User')}}</label>
-                              <input type="email" id="form2Example11" class="form-control"
-                                placeholder="Phone number or email address" />
+                                            <!-- Email Address -->
+                                            <div>
+                                                <x-input-label for="email" :value="__('Email')" />
+                                                <x-text-input id="email" class="form-control" type="email"
+                                                    name="email" :value="old('email')" required autofocus
+                                                    autocomplete="username" />
+                                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                            </div>
+
+                                            <!-- Password -->
+                                            <div class="mt-4">
+                                                <x-input-label for="password" :value="__('Password')" />
+                                                <x-text-input id="password" class="form-control" type="password"
+                                                    name="password" required autocomplete="current-password" />
+                                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                            </div>
+
+                                            <!-- Remember Me -->
+                                            <div class="block mt-4">
+                                                <label for="remember_me"
+                                                    class="inline-flex items-center">
+                                                    <input id="remember_me" type="checkbox"
+                                                        class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                                                        name="remember">
+                                                    <span
+                                                        class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                                                </label>
+                                            </div>
+
+                                            <div class="flex items-center justify-end mt-4">
+                                                @if (Route::has('password.request'))
+                                                <a
+                                                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                                                    href="{{ route('password.request') }}">
+                                                    {{ __('Forgot your password?') }}
+                                                </a>
+                                                @endif
+
+                                                <button
+                                                    class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+                                                    type="submit">{{__('Log in')}}</button>
+                                            </div>
+                                        </form>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                        <h4 class="mb-4">We are more than just a company</h4>
+                                        <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing
+                                            elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                                            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                            aliquip ex ea commodo consequat.</p>
+                                    </div>
+                                </div>
                             </div>
-
-                            <div class="form-outline mb-4">
-                              <label class="form-label" for="form2Example22">{{__('Password')}}</label>
-                              <input type="password" id="form2Example22" class="form-control" />
-                            </div>
-
-                            <div class="text-center pt-1 mb-5 pb-1">
-                              <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">{{__('Submit')}}</button>
-                              <a class="text-muted" href="#!">{{__('Forgot your password?')}}</a>
-                            </div>
-
-                            <div class="d-flex align-items-center justify-content-center pb-4">
-                              <p class="mb-0 me-2">{{__('Don\'t have an account?')}}</p>
-                              <a href="{{route('register')}}" class="btn btn-outline-danger">{{__('Create')}}</a>
-                            </div>
-
-                          </form>
-
                         </div>
-                      </div>
-                      <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                        <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                          <h4 class="mb-4">We are more than just a company</h4>
-                          <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        </div>
-                      </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </section>
+        </section>
 
-        <script
-            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
             integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-            crossorigin="anonymous"
-        ></script>
+            crossorigin="anonymous"></script>
 
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
             integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-            crossorigin="anonymous"
-        ></script>
+            crossorigin="anonymous"></script>
     </body>
-</html>
 
+    </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,95 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Title</title>
+            <!-- Required meta tags -->
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+            <!-- Bootstrap CSS v5.2.1 -->
+            <link
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+                rel="stylesheet"
+                integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+                crossorigin="anonymous"
+            />
+            <link rel="stylesheet" href="{{ asset('assets/estilos.css') }}">
+        </head>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+        <body>
+            <section class="h-100 gradient-form" style="background-color: #eee;">
+                <div class="container py-5 h-100">
+                    <div class="row d-flex justify-content-center align-items-center h-100">
+                        <div class="col-xl-10">
+                            <div class="card rounded-3 text-black">
+                                <div class="row g-0">
+                                    <div class="col-lg-6">
+                                        <div class="card-body p-md-5 mx-md-4">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+                                            <div class="text-center">
+                                                <img src="{{ asset('assets/img/logo.jpg')}}" style="width: 185px;" alt="logo">
+                                                <h4 class="mt-1 mb-5 pb-1">{{__('Login')}}</h4>
+                                            </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                                            <form method="POST" action="{{ route('register') }}">
+                                                @csrf
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                                                <!-- Name -->
+                                                <div>
+                                                    <x-input-label for="name" :value="__('Name')" />
+                                                    <x-text-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                                                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                                </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                                                <!-- Email Address -->
+                                                <div class="mt-4">
+                                                    <x-input-label for="email" :value="__('Email')" />
+                                                    <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                                </div>
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                                                <!-- Password -->
+                                                <div class="mt-4">
+                                                    <x-input-label for="password" :value="__('Password')" />
+                                                    <x-text-input id="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
+                                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                                </div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                                                <!-- Confirm Password -->
+                                                <div class="mt-4">
+                                                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                                                    <x-text-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
+                                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                                </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+                                                <div class="flex items-center justify-end mt-4">
+                                                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                                                        {{ __('Already registered?') }}
+                                                    </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+                                                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">{{__('Register')}}</button>
+                                                </div>
+                                            </form>
+
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                        <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                            <h4 class="mb-4">We are more than just a company</h4>
+                                            <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+        </body>
+    </html>
